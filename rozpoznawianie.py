c3ed3582dba07a5acfa25e4d73b4e8c1042b83b0
Piotr Sawicki <piotrsawicki13@gmail.com>
2025-11-27 23:58:07 +0100

    próba implementacji zaznaczania odcinka na zdjęciu

--- rozpoznawianie.py
+++ rozpoznawianie.py
@@ -1,12 +1,9 @@
 import cv2 as cv
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 scale = 0.4
-
-img_path = ".//photos/IMG_20221011_172837.jpg"
-print(img_path)
-
 
 # rozmiar bilonu w Polsce
 grosz = 15.5
@@ -19,19 +16,50 @@ zlotowka = 23
 dwa_zlote = 21.5
 piec_zlotych = 24
 
+
+
+def mouse_callback(event, x, y, flags, param):
+    if event == cv.EVENT_LBUTTONDOWN:
+        param["lista"].append((x,y))
+        param["points"] += 1
+        cv.circle(param["img"], (x, y), 5, (0, 0, 255), -1)
+
+
+img_path = ".//photos/IMG_20221011_172837.jpg"
+print(img_path)
+
 img = cv.imread(img_path, 0)
-img = cv.resize(img,None, fx=scale, fy=scale)
+img = cv.resize(img, None, fx=scale, fy=scale)
 img = cv.rotate(img, cv.ROTATE_90_CLOCKWISE)
-img = cv.medianBlur(img,3)
-cimg = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
- 
-cv.imshow('detected circles',img)
-cv.waitKey(0)
-cv.destroyAllWindows()
- 
+img = cv.medianBlur(img, 3)
+cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+param = {"points": 0, "lista": [], "img": img}
+if img is None:
+    print("Nie udało się wczytać obrazu!")
+    exit()
+
+
+while True:
+    cv.imshow("Zaznacz odcinek 5cm na obrazku",img)
+    if param["points"] == 2:
+        cv.destroyAllWindows()
+        break
+
+
+    cv.setMouseCallback("Zaznacz odcinek 5cm na obrazku", mouse_callback, param)
+
+
+
+    key = cv.waitKey(1) & 0xFF
+    if key == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print(param["lista"])
+
 circles = cv.HoughCircles(img,cv.HOUGH_GRADIENT,1,200,
 param1=150,param2=130,minRadius=50,maxRadius=500)
- 
+
 circles = np.uint16(np.around(circles))
 print(f"Ilość kół: {circles}")
 for i in circles[0,:]:
@@ -39,8 +67,11 @@ for i in circles[0,:]:
     cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
 
- 
+
 cv.imshow('detected circles',cimg)
 cv.waitKey(0)
 cv.destroyAllWindows()
 cv.imwrite('wynik_detekcji.png',cimg)
+
+
+# if __name__ == '__main__':
